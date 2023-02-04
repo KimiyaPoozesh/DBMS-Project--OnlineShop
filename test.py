37fd4c3115d41bb2ade619297b85fb31ca44e11f
KimiyaPoozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2023-02-04 12:13:21 +0330

    can now choose a name to query with

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ mydb1 = mysql.connector.connect(host='localhost',user='root',passwd='123kimiya45
 a=[]
 
 mycuresor = mydb1.cursor()
+cursor = mydb1.cursor(prepared=True)
 
 print("hello, choose a number for the following queries: ")
 print("1. Display Users lists-")
@@ -16,30 +17,37 @@ print("3. Display the last ten orders of the user")
 print("4. Display users from the same city")
 
 
-def query1():
-    mycuresor.execute("SELECT userName,fName from user")
-def query2():
-    mycuresor.execute(" SELECT k.city,GROUP_CONCAT(DISTINCT k.fName) FROM user AS k INNER JOIN user as d ON k.city = d.city GROUP BY k.city")
-#have to add variable
-def query3():
-    mycuresor.execute("SELECT fName,p.name from product as p, customer as c,user as u,basket as b,basketitem as bi WHERE.\
-        u.fName = 'mostafa' AND bi.BasketID = b.BasketID AND b.BasketID = c.basketID AND c.customerID=u.ID")
-def query4():
-    mycuresor.execute(" SELECT k.city,GROUP_CONCAT(DISTINCT k.fName) FROM user AS k INNER JOIN user as d ON k.city = d.city GROUP BY k.city")
+
+
+def executor(query):
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.nextset()
+    return result
+
+def executor2(query,var):
+    cursor.execute(query,(var,))
+    result = cursor.fetchall()
+    cursor.nextset()
+    return result
 
 class Choice(BaseModel):
     choice: int
+    name: str
+
+
 @app.post('/')
 def index(choice : Choice):
-    if choice == 1:
-        query1()
-    elif choice ==2:
-        query2()
-    elif choice == 3:
-        query3()
+    if choice[0] == 1:
+        r=executor("SELECT userName,fName from user")
+    elif choice[0] ==2:
+        r=executor(" SELECT k.city,GROUP_CONCAT(DISTINCT k.fName) FROM user AS k INNER JOIN user as d ON k.city = d.city GROUP BY k.city")
+    elif choice[0] == 3:
+        r=executor2("""SELECT fName,p.name from product as p, customer as c,user as u,basket as b,basketitem as bi WHERE.\
+        u.fName = %s AND bi.BasketID = b.BasketID AND b.BasketID = c.basketID AND c.customerID=u.ID""",choice[1])
     else:
-        query4()
-    for db in mycuresor:
+        r=executor(" SELECT k.city,GROUP_CONCAT(DISTINCT k.fName) FROM user AS k INNER JOIN user as d ON k.city = d.city GROUP BY k.city")
+    for db in r:
         a.append(db)
     return a
 
