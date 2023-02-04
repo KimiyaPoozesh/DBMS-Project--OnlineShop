fe07fe7349ea0bcaee62b2197a3a87eba3632e97
KimiyaPoozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2023-02-04 12:22:27 +0330

    inserted some query

--- mysql connector.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(host="localhost",user="root"
-                               ,passwd="123")
-if (mydb):
-    print("Successful")
-else:
-    print("unsSuccessful")
